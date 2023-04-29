0da9830c8cdf612feb4f96d3c2e911dfc702074f
VadimKorobec <_s_e_t__@ukr.net>
2023-04-29 18:34:03 +0300

    Update Product.jsx

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,19 @@
-export const Product = ({ imgUrl, name, price }) => {
+import PropTypes from 'prop-types';
+
+export const Product = ({ imgUrl, name, price, quantity }) => {
   return (
     <div>
       <img src={imgUrl} alt={name} width="640" />
       <h2>{name}</h2>
       <p>Price:{price} $</p>
+      <h1>Ouantity:{quantity < 20 ? 'Few left ' : 'in stock'}</h1>
       <button type="button">Add to cart</button>
     </div>
   );
+};
+
+Product.propTypes = {
+  imgUrl: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 };
