84db0bfce510be7f8b9e98530bc89b493654442b
VadimKorobec <_s_e_t__@ukr.net>
2023-05-01 18:54:27 +0300

    Create Counter.styled.jsx

---
+++ src/components/Counter/Counter.styled.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Increment = styled.button`
+  background-color: green;
+  border: none;
+  padding: 5px 5px;
+  border-radius: 4px;
+`;
+
+export const Decrement = styled.button`
+  background-color: tomato;
+  border: none;
+  padding: 5px 5px;
+  border-radius: 4px;
+`;
+
+export const Sum = styled.span`
+  font-size: 50px;
+  margin: 0px 5px;
+`;
