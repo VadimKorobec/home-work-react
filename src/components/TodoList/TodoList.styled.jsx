65c8f8deca0c5aa69ba426020eae9dde04e029b7
VadimKorobec <_s_e_t__@ukr.net>
2023-05-03 14:42:36 +0300

    Create TodoList.styled.jsx

---
+++ src/components/TodoList/TodoList.styled.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Countainer = styled.div`
+  width: 200px;
+  height: 200px;
+  padding: 10px 10px;
+`;
+
+export const Todo = styled.li`
+  display: flex;
+  gap: 50px;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+  border: 1px solid tomato;
+  border-radius: 4px;
+`;
