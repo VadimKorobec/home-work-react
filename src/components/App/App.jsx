d6eba646a3486f1f1b4c3380b2c205786a9163d6
VadimKorobec <_s_e_t__@ukr.net>
2023-05-03 14:42:52 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,10 +8,15 @@ import { ProfileUser } from 'components/ProfileUser/ProfileUser';
 import user from '../data/user.json';
 import stats from '../data/data.json';
 import friends from '../data/friends.json';
+import initialTodos from '../data/todos.json';
 import { StatiticsList } from 'components/StatisticsList/StatisticsList';
 import { FriendsList } from 'components/FriendsList/FriendsList';
 import { Counter } from 'components/Counter/Counter';
 import { Toggle } from 'components/Toggle/Toggle';
+import { Dropdown } from 'components/Dropdown/Dropdown';
+import { ColorPicker } from 'components/ColorPicker/ColorPicker';
+import { TodoList } from 'components/TodoList/TodoList';
+import { Component } from 'react';
 
 const favoriteBooks = [
   { id: 'id-1', name: 'JS for beginners', price: 10 },
@@ -19,41 +24,70 @@ const favoriteBooks = [
   { id: 'id-3', name: 'React Router overview', price: 30 },
   { id: 'id-4', name: 'Redux in depth', price: 40 },
 ];
+const colorPicker = [
+  { label: 'red', color: '#f44336' },
+  { label: 'green', color: '#4caf50' },
+  { label: 'blue', color: '#2196f3' },
+  { label: 'grey', color: '#687d8b' },
+  { label: 'pink', color: '#e91e62' },
+  { label: 'indigo', color: '#3f51b5' },
+];
 
-export const App = () => {
-  return (
-    <div>
-      <Toggle />
-      <Counter />
-      <FriendsList friends={friends} />
-      <StatiticsList stats={stats} />
-      <ProfileUser user={user} />
-      <Alert variant="info">Please update your email! </Alert>
-      <Alert variant="error">There was an error during transaction </Alert>
-      <Alert variant="success">
-        Payment received,thank you for your purchase!
-      </Alert>
-      <Alert variant="warning">
-        Please update your profile contact information
-      </Alert>
-      <h1>Best selling products</h1>
-      <Product
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        name="Tacos With Lime"
-        price={10.99}
-        quantity={10}
-      />
-      <Product
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        name="Fries and Burger"
-        price={14.29}
-        quantity={30}
-      />
-      <Panel titel={'User profil'}>
-        <Profile name={'Mango'} email={'mango@mail.com'} />
-      </Panel>
-      <Mailbox unreadMessages={false} username={'Mango'} />
-      <BookList books={favoriteBooks} />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <div>
+        <div>
+          <p>Todo:{todos.length}</p>
+          <p>Todo:</p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        <ColorPicker colors={colorPicker} />
+        <Dropdown />
+        <Toggle />
+        <Counter initialValue={0} />
+        <FriendsList friends={friends} />
+        <StatiticsList stats={stats} />
+        <ProfileUser user={user} />
+        <Alert variant="info">Please update your email! </Alert>
+        <Alert variant="error">There was an error during transaction </Alert>
+        <Alert variant="success">
+          Payment received,thank you for your purchase!
+        </Alert>
+        <Alert variant="warning">
+          Please update your profile contact information
+        </Alert>
+        <h1>Best selling products</h1>
+        <Product
+          imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+          name="Tacos With Lime"
+          price={10.99}
+          quantity={10}
+        />
+        <Product
+          imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+          name="Fries and Burger"
+          price={14.29}
+          quantity={30}
+        />
+        <Panel titel={'User profil'}>
+          <Profile name={'Mango'} email={'mango@mail.com'} />
+        </Panel>
+        <Mailbox unreadMessages={false} username={'Mango'} />
+        <BookList books={favoriteBooks} />
+      </div>
+    );
+  }
+}
