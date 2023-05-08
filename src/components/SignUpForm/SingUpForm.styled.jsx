341388ff6a515b87390c320095e9adc4cbc55715
VadimKorobec <_s_e_t__@ukr.net>
2023-05-08 18:58:45 +0300

    Create SingUpForm.styled.jsx

---
+++ src/components/SignUpForm/SingUpForm.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 500px;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  justify-content: space-between;
+`;
