79f5183cdaabd0010cb1def944d74643e29dfbda
VadimKorobec <_s_e_t__@ukr.net>
2023-05-08 18:58:48 +0300

    Create LoginForm.styled.jsx

---
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Forma = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 10px;
+`;
